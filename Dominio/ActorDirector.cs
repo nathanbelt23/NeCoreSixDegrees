f731fd8390caef9c758e76b42889dc8093a22348
Yonathan Beltran Romero <nathanbelt23@gmail.com>
2021-05-10 11:14:59 -0500

    Add project files.

---
+++ Dominio/ActorDirector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Dominio
+{
+    public class ActorDirector
+    {
+        [Key]
+        public int ActorDirectorID { get; set; }
+        public string Nombre { get; set; }
+       
+        public bool EsActor { get; set; }
+        public bool EsDirector { get; set; }
+
+        public string? urlFoto { get; set; }
+        [NotMapped]
+        public byte[] FotoActorDirector { get; set; }
+
+        [NotMapped]
+        public bool Seleccionado { get; set; }
+
+
+        ICollection<PeliculaActor> Peliculalnk { get; set; }
+        ICollection<PeliculaDirector> PeliculaDirectorlnk { get; set; }
+
+    }
+}
